f0f2f99ef1ccae4b60d56b374ac42c230ea9ab83
Martin Eduardo Camilo De La Cruz <martin_camilo@ucne.edu.do>
2020-06-26 07:03:51 -0400

    DAL creada y contexto terminado

---
+++ SuplidoresBlazor/DAL/Contexto.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuplidoresBlazor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuplidoresBlazor.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=DATA\Suplider.db");
+        }
+
+        public DbSet<Ordenes> Ordenes { get; set; }
+        public DbSet<Productos> Productos { get; set; }
+        public DbSet<Suplidores> Suplidores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Productos>().HasData(new Productos
+            {
+                productoId = 1,
+                descripcion = "Cacao",
+                costo = 100,
+                inventario = 0
+            });
+
+            modelBuilder.Entity<Suplidores>().HasData(new Suplidores
+            {
+                suplidorId = 1,
+                nombre = "Rizek"
+            });
+
+            modelBuilder.Entity<Productos>().HasData(new Productos
+            {
+                productoId = 2,
+                descripcion = "Chocolate",
+                costo = 100,
+                inventario = 0
+            });
+
+            modelBuilder.Entity<Suplidores>().HasData(new Suplidores
+            {
+                suplidorId = 2,
+                nombre = "Supremo"
+            });
+
+        }
+    }
+}
